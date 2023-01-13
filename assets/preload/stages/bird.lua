6476f6ce65f5a4a4c3339656a10ec8d73dc7ab3e
BieuGuys <114687628+BieuGuys@users.noreply.github.com>
2023-01-13 11:25:14 -0300

    Add files via upload

---
+++ assets/preload/stages/bird.lua
@@ -0,0 +1,75 @@
+
+local u = false;
+local r = 0;
+local shot = false;
+local xx = 420.95;
+local yy = 313;
+local xx2 = 952.9;
+local yy2 = 350;
+local ofs = 50;
+local followchars = true;
+local del = 0;
+local del2 = 0;
+
+function onUpdate()
+	if del > 0 then
+		del = del - 1
+	end
+	if del2 > 0 then
+		del2 = del2 - 1
+	end
+    if followchars == true then
+        if mustHitSection == true then
+            setProperty('defaultCamZoom',0.8)
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+               triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+               triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+               triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+		end
+    end
+    
+end
+
+function onBeatHit()
+
+end
+
+
+function onEvent(name,value1,value2)
+
+end
+function onCreate()
+
+    makeLuaSprite('birdbackground', 'birdb/birdbackground', -700, -300)
+    setScrollFactor('birdbackground', 0.9, 0.9);
+    
+    addLuaSprite('birdbackground', false)
+end
+    
